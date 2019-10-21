b409909157bcad4a16d3eb489d9e13313f627eff
hyeji <dlgpwl00@gmail.com>
2019-10-21 17:46:38 +0900

    up data

---
+++ e_example/js/jquery/woori_senior_plus.js
@@ -0,0 +1,24 @@
+// woori_senior_plus.js
+
+(function($){
+  const gnb = $('#gnb');
+  const gnbUl = gnb.children('ul');
+  const gnbDd = gnb.find('dd');
+
+  gnbUl.on('mouseenter', function(){
+    gnbDd.stop().slideDown();
+  });
+
+  gnbUl.on('mouseleave', function(){
+    gnbDd.stop().slideUp();
+  });
+
+  gnbUl.find('a').on('focus', function(){
+    gnbDd.stop().slideDown();
+  });
+
+  gnbUl.find('a').on('blur', function(){
+    gnbDd.stop().slideUp();
+  });
+  
+})(jQuery);
